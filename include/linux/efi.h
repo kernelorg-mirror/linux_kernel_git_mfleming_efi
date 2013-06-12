66d6de56c01e2960464033126798ba094bcf6afc
Matt Fleming <matt.fleming@intel.com>
2013-06-12 09:32:03 +0100

    efi: move efi_status_to_err() to efi.h
    Move efi_status_to_err() into the efi.h header as it's generally useful
    in all bits of EFI code where there is a need to convert an efi_status_t
    to a kernel error value.
    
    Signed-off-by: Matt Fleming <matt.fleming@intel.com>

--- include/linux/efi.h
+++ include/linux/efi.h
@@ -741,6 +741,39 @@ static inline void memrange_efi_to_native(u64 *addr, u64 *npages)
 	*addr &= PAGE_MASK;
 }
 
+static inline int efi_status_to_err(efi_status_t status)
+{
+	int err;
+
+	switch (status) {
+	case EFI_SUCCESS:
+		err = 0;
+		break;
+	case EFI_INVALID_PARAMETER:
+		err = -EINVAL;
+		break;
+	case EFI_OUT_OF_RESOURCES:
+		err = -ENOSPC;
+		break;
+	case EFI_DEVICE_ERROR:
+		err = -EIO;
+		break;
+	case EFI_WRITE_PROTECTED:
+		err = -EROFS;
+		break;
+	case EFI_SECURITY_VIOLATION:
+		err = -EACCES;
+		break;
+	case EFI_NOT_FOUND:
+		err = -ENOENT;
+		break;
+	default:
+		err = -EINVAL;
+	}
+
+	return err;
+}
+
 /*
  * EFI Variable support.
  *
