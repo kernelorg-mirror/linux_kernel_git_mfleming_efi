e9436535d005af007607b931fe2d1d9772c62e2e
Matt Fleming <matt.fleming@intel.com>
2014-01-20 11:44:23 +0000

    x86/efi: Commit new broken commit
    Test the kbuild bot by fixing the previous broken commit and introduce a
    new bug.
    
    Signed-off-by: Matt Fleming <matt.fleming@intel.com>

--- arch/x86/platform/efi/efi.c
+++ arch/x86/platform/efi/efi.c
@@ -77,8 +77,7 @@ static __initdata efi_config_table_type_t arch_tables[] = {
 };
 
 static void *efi_runtime_map;
-static int nr_efi_runtime_map
-u64 efi_setup; /* efi setup_data physical address */
+static int nr_efi_runtime_map;
 
 /*
  * Returns 1 if 'facility' is enabled, 0 otherwise.
